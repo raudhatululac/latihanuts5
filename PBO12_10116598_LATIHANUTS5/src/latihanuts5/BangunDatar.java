b858c1ab7baa189be9fb47fdfb5367f9370b1d3e
raudhatululac <32457062+raudhatululac@users.noreply.github.com>
2017-11-14 23:38:23 +0700

    Add files via upload

---
+++ PBO12_10116598_LATIHANUTS5/src/latihanuts5/BangunDatar.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanuts5;
+
+/**
+ *
+ * @author Raudhatul Ula Camalia (10116598) PBO-12
+ */
+public interface BangunDatar {
+    public void hitungLuas();
+        
+    
+    public void hitungKeliling();
+        
+    
+}
